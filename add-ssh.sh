058b5bbb46faf01055742928f5c7b205b855cc0d
rbantug <ronbantug22@gmail.com>
2026-06-05 01:03:57 +0800

    init

---
+++ add-ssh.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ssh-keygen -t ed25519 -N "" -f ~/.ssh/github_repo_key -C "secure-dev"
+
+cat > ~/.ssh/config << 'EOF'
+Host github-project
+    HostName github.com
+    User git
+    IdentityFile ~/.ssh/github_repo_key
+    IdentitiesOnly yes
+EOF
+
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/github_repo_key
+chmod 644 ~/.ssh/github_repo_key.pub
+chmod 600 ~/.ssh/config
+
+echo public ssh key:
+cat ~/.ssh/github_repo_key.pub
